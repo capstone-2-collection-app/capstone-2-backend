72c9c6c96a862b9efc1c3fee1a54d8d6065f466c
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-08-03 11:06:47 -0400

    cors change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://capstone-2-frontend-gilt.vercel.app",
+  "https://capstone-2-frontend-v2.vercel.app",
 ];
 
 app.use(express.json());
