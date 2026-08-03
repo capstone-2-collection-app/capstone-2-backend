97fde8d0b93cc234025fac60f94ba6bab3f84203
anxgelll <angel.ventura6251@gmail.com>
2026-08-02 22:04:11 -0400

    Allow local frontend requests

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ const errorHandler = require("./middleware/errorHandler.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://capstone-2-frontend-gilt.vercel.app",
+];
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://capstone-2-frontend-gilt.vercel.app",
+    origin: allowedOrigins,
     credentials: true,
   }),
 );
